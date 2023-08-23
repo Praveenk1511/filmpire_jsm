d51f6434f132b08b7f8ea5237b789db416a17b21
praveen <praveen.k@mapmyindia.com>
2023-08-23 16:30:17 +0530

    commit 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import {Route , Switch} from 'react-router-dom';
+
+import { Actor, MovieInformation, Movies, NavBar, Profile} from './';
 
 const App = () => {
   return (
     <div>
-      Filmpire App
+    <CssBaseline /> 
+    <NavBar />
+    <main>
+      <Switch>
+         <Route exact path="/"><Movies></Movies></Route>
+         {/* <Route exact path="/movies"></Route> */}
+         <Route exact path="/movies/:id"><MovieInformation /></Route>
+         <Route exact path="/profile/:id"><Profile /></Route>
+         <Route exact path="/actor/:id"><Actor /></Route>
+      </Switch>
+    </main>
     </div>
   )
 }
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Actor } from './Actor/Actor';
+export { default as Movies } from './Movies/Movies';
+export { default as MovieInformation } from './MovieInformation/MovieInformation';
+export { default as Profile } from './Profile/Profile';
+export { default as NavBar } from './NavBar/NavBar';
